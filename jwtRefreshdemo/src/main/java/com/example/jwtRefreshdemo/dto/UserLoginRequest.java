efeed04c7dee591ff5dc06024a52592f1734950a
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-20 18:52:31 +0530

    add validation in login request fto

--- jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/dto/UserLoginRequest.java
+++ jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/dto/UserLoginRequest.java
@@ -1,9 +1,15 @@
 package com.example.jwtRefreshdemo.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserLoginRequest {
+
+    @NotBlank(message  = "Username is mandatory")
     private String username;
+
+    @NotBlank(message = "Password is mandatory")
     private String password;
+
 }
