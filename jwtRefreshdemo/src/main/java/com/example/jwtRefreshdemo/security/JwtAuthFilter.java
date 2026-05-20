d5c81faf4824c9b0de1e54752250ba433e80ae1c
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-20 18:56:33 +0530

    fix critical bug

--- jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/security/JwtAuthFilter.java
+++ jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/security/JwtAuthFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,14 +21,15 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtils jwtUtils;
     private final CustomUserDetailsService userDetailsService;
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         try{
             final String authHeader = request.getHeader("Authorization");
             if(authHeader == null || !authHeader.startsWith("Bearer ") ){
                 filterChain.doFilter(request,response);
+                return;
             }
             final String accessToken = authHeader.substring(7);
             final String username = jwtUtils.extractUserName(accessToken);
@@ -47,7 +49,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             filterChain.doFilter(request,response);
         }
         catch (Exception e){
-                throw new ServletException("error");
+            throw new ServletException("error",e);
         }
     }
 }
